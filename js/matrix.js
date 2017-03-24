7f7251b549800e7cec218aa12ddfd7f9d26d3603
Kingsley Ochu <kochu@c2gconsulting.com>
2017-03-24 12:35:46 +0100

    Refactor matrix

--- js/matrix.js
+++ js/matrix.js
@@ -1,257 +1,48 @@
-$(function() {
-    // RequestAnimFrame: a browser API for getting smooth animations
-    window.requestAnimFrame = (function() {
-        return window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function(callback) {
-                window.setTimeout(callback, 1000 / 5);
-            };
-    })();
+(function() {
+    var c = document.getElementById("matrix");
+    var ctx = c.getContext("2d");
 
-    // Initializing the canvas
-    // I am using native JS here, but you can use jQuery, 
-    // Mootools or anything you want
-    var canvas = document.getElementById("matrix");
+    //making the canvas full screen
+    c.height = window.innerHeight;
+    c.width = window.innerWidth;
 
-    // Initialize the context of the canvas
-    var ctx = canvas.getContext("2d");
+    var characters = "<code>/!{}[]☺";
+    characters = characters.split("");
 
-    // Set the canvas width and height to occupy full window
-    var W = window.innerWidth,
-        H = window.innerHeight;
-    canvas.width = W;
-    canvas.height = H;
-    var m = { x: 0, y: 0 };
+    var font_size = 13;
+    var columns = c.width / font_size; //number of columns for the rain
+    //an array of drops - one per column
+    var drops = [];
 
-    //Get Mouse position
-    canvas.addEventListener('mousemove', function(e) {
+    //x below is the x coordinate
+    //1 = y co-ordinate of the drop(same for every drop initially)
+    for (var x = 0; x < columns; x++)
+        drops[x] = 1;
 
-        bounds = canvas.getBoundingClientRect();
-        m.x = e.clientX - bounds.left;
-        m.y = e.clientY - bounds.top;
-
-    });
-
-    // Some variables for later use
-    var particleCount = 100,
-        particles = [],
-        minDist = 70,
-        dist;
-
-    // Function to paint the canvas black
-    function paintCanvas() {
-        // This will create a rectangle of white color from the 
-        // top left (0,0) to the bottom right corner (W,H)
-        ctx.fillStyle = "rgba(20,20,20,1)";
-        ctx.fillRect(0, 0, W, H);
-    }
-
-    // Now the idea is to create some particles that will attract
-    // each other when they come close. We will set a minimum
-    // distance for it and also draw a line when they come
-    // close to each other.
-
-    // The attraction can be done by increasing their velocity as 
-    // they reach closer to each other
-
-    // Let's make a function that will act as a class for
-    // our particles.
-
-    function Particle() {
-        // Position them randomly on the canvas
-        // Math.random() generates a random value between 0
-        // and 1 so we will need to multiply that with the
-        // canvas width and height.
-        this.x = Math.random() * W;
-        this.y = Math.random() * H;
-
-        // Now the radius of the particles. I want all of 
-        // them to be equal in size so no Math.random() here..
-        this.radius = 3;
-
-        // We would also need some velocity for the particles
-        // so that they can move freely across the space
-        this.maxX = W - this.radius;
-        this.maxY = H - this.radius;
-
-        this.targetX = Math.random() * this.maxX;
-        this.targetY = Math.random() * this.maxY;
-
-        var speed = Math.random() * 2000 + 2000;
-        this.vx = (this.targetX - this.x) / speed;
-        this.vy = (this.targetY - this.y) / speed;
-
-        // This is the method that will draw the Particle on the
-        // canvas. It is using the basic fillStyle, then we start
-        // the path and after we use the `arc` function to 
-        // draw our circle. The `arc` function accepts four
-        // parameters in which first two depicts the position
-        // of the center point of our arc as x and y coordinates.
-        // The third value is for radius, then start angle, 
-        // end angle and finally a boolean value which decides
-        // whether the arc is to be drawn in counter clockwise or 
-        // in a clockwise direction. False for clockwise.
-        this.draw = function() {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            ctx.fillStyle = "rgba(39, 174, 96,1.0)";
-
-            // Fill the color to the arc that we just created
-            ctx.fill();
-        }
-    }
-
-    // Time to push the particles into an array
-    for (var i = 0; i < particleCount; i++) {
-        particles.push(new Particle());
-    }
-
-    // Function to draw everything on the canvas that we'll use when 
-    // animating the whole scene.
     function draw() {
+        //Black BG for the canvas
+        //translucent BG to show trail
+        ctx.fillStyle = "rgba(0, 0, 0, 0.06)";
+        ctx.fillRect(0, 0, c.width, c.height);
 
-        // Call the paintCanvas function here so that our canvas
-        // will get re-painted in each next frame
-        paintCanvas();
+        ctx.fillStyle = "#0F0"; //green text
+        ctx.font = font_size + "px arial";
+        //looping over drops
+        for (var i = 0; i < drops.length; i++) {
+            //a random chinese character to print
+            var text = characters[Math.floor(Math.random() * characters.length)];
+            //x = i*font_size, y = value of drops[i]*font_size
+            ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-        // Call the function that will draw the balls using a loop
-        for (var i = 0; i < particles.length; i++) {
-            p = particles[i];
-            p.draw();
-        }
+            //sending the drop back to the top randomly after it has crossed the screen
+            //adding a randomness to the reset to make the drops scattered on the Y axis
+            if (drops[i] * font_size > c.height && Math.random() > 0.975)
+                drops[i] = 0;
 
-        //Finally call the update function
-        update();
-    }
-
-    // Give every particle some life
-    function update() {
-
-        // In this function, we are first going to update every
-        // particle's position according to their velocities
-        if (runAnimation) {
-            for (var i = 0; i < particles.length; i++) {
-                p = particles[i];
-                popClose(p, m, i);
-
-                if (p.frozen) {
-                    p.radius = 10;
-                }
-
-                if (!p.frozen) {
-
-                    //Set default velocity
-                    p.radius = 3;
-
-                    // Change the velocities
-                    p.x += p.vx;
-                    p.y += p.vy;
-
-                    // We don't want to make the particles leave the
-                    // area, so just change their position when they
-                    // touch the walls of the window
-
-                    if (p.x > W || p.x < 0) {
-                        p.vx = -p.vx;
-                        p.x += p.vx;
-                    } else if (p.y || p.y < 0) {
-                        p.vy = -p.vy;
-                        p.y += p.vy
-                    }
-                }
-
-                // Now we need to make them attract each other
-                // so first, we'll check the distance between
-                // them and compare it to the minDist we have
-                // already set
-
-                // We will need another loop so that each
-                // particle can be compared to every other particle
-                // except itself
-                for (var j = i + 1; j < particles.length; j++) {
-                    p2 = particles[j];
-                    distance(p, p2);
-                }
-            }
+            //incrementing Y coordinate
+            drops[i]++;
         }
     }
 
-    // Distance calculator between two particles
-    function distance(p1, p2) {
-        var dist,
-            dx = Math.abs(p1.x - p2.x);
-        dy = Math.abs(p1.y - p2.y);
-
-        dist = Math.sqrt(dx * dx + dy * dy);
-
-        // Draw the line when distance is smaller
-        // then the minimum distance
-        if (dist <= minDist) {
-
-            // Draw the line
-            ctx.beginPath();
-            ctx.strokeStyle = "rgba(46, 204, 113,1.0)";
-            ctx.moveTo(p1.x, p1.y);
-            ctx.lineTo(p2.x, p2.y);
-            ctx.stroke();
-            ctx.closePath();
-
-            // Some acceleration for the partcles 
-            // depending upon their distance
-            // var ax = dx/2000,
-            //   ay = dy/2000;
-
-            // Apply the acceleration on the particles
-            // p1.vx = p2.vx;
-            // p1.vy = p2.vy;
-        }
-    }
-
-    var closestIndex;
-
-    function popClose(p, m, i) {
-        var dist,
-            dx = Math.abs(p.x - m.x);
-        dy = Math.abs(p.y - m.y);
-
-        dist = Math.sqrt(dx * dx + dy * dy);
-
-        // Draw the line when distance is smaller
-        // then the minimum distance
-        if (dist <= p.radius * 1.5) {
-            closestIndex = i;
-            particles[closestIndex].frozen = true;
-        } else {
-            if (closestIndex != undefined) {
-                particles[closestIndex].frozen = false;
-            }
-        }
-    }
-
-    canvas.addEventListener('mousedown', function(e) {
-        defaultRadius = 3;
-        var links = ["model", "market-pains", "ds-solutions", "history", "clients"]
-        var link = Math.floor(Math.random() * 4);
-
-        if (defaultRadius < particles[closestIndex].radius) {
-            $(document.body).animate({
-                'scrollTop': $('#' + links[link]).offset().top
-            }, 500);
-        }
-    });
-
-    // Start the main animation loop using requestAnimFrame
-    function animloop() {
-        draw();
-        requestAnimFrame(animloop);
-    }
-
-
-    //Custom functions
-    var runAnimation = true;
-
-    animloop();
-});
+    setInterval(draw, 100);
+})()
